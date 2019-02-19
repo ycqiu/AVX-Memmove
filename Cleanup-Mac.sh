76ec8c27a1d053755653ecb4a3bbfadb066dfdbe
KNN <KNNSpeed@users.noreply.github.com>
2019-02-18 23:07:10 -0500

    Add Linux and Mac cleanup scripts

---
+++ Cleanup-Mac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set +v
+
+echo
+echo "Running cleanup procedures; press CTRL+C to exit now. Otherwise..."
+read -n1 -r -p "Press any key to continue..."
+
+rm Output.mach64
+rm output.map
+
+rm *.o
+rm *.d
+#rm *.out
+
+echo
+echo "Done! All clean."
+echo
